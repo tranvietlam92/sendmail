9aa3c5f990f64afe3add4e07c7907aca61cfebd5
Tran Viet Lam <lamtv@fds.vn>
2020-05-18 14:36:32 +0700

    update source code

--- SendMail/src/main/java/org/opencps/utils/ConstantsUtils.java
+++ SendMail/src/main/java/org/opencps/utils/ConstantsUtils.java
@@ -2,9 +2,6 @@ package org.opencps.utils;
 
 public class ConstantsUtils {
 
-	//public static String MAIL_ADMIN_SERVER = "motcong@mofa.gov.vn";
-	//public static String NAME_ADMIN_SERVER = "He thong dich vu cong Bo Ngoai Giao";
-	//public static String BASE_URL = "https://bpmc.mofa.gov.vn";
 	public static final String SUB_URL_ACTIVE = "register#/xac-thuc-tai-khoan?active_user_id=";
 	public static final String PROPERETIES_CONFIG_PATH = "/opt/SendMail/config.properties";
 }
